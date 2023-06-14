c9db5e0e642237d81906a22ab0c20a9d73c92775
Black-Hole1 <bh@bugs.cc>
2023-06-14 10:46:59 +0800

    chore(command): add version

--- cmd/b/main.go
+++ cmd/b/main.go
@@ -5,12 +5,15 @@ import (
 
 	"go.uber.org/zap"
 
+	"github.com/BlackHole1/b/internal/version"
 	"github.com/BlackHole1/b/pkg/log"
 )
 
 func main() {
 	defer log.Sync()
+
 	rootCmd.AddCommand(asarCmd)
+	rootCmd.Version = version.Version
 
 	if err := rootCmd.Execute(); err != nil {
 		log.Error("failed to execute command", zap.Error(err))
